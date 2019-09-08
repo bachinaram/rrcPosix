2743ce25077788c1315abbd57a05a73de4db423b
bachinaram <55042314+bachinaram@users.noreply.github.com>
2019-09-09 02:44:00 +0530

    adding missed headr file changes

--- operations.h
+++ operations.h
@@ -1,8 +1,14 @@
 #include<string>
 using namespace std;
 string readEnvFile(string promptString);
+int readEnvFile1(string promptString);
 void historyRecord(string inputCommand);
 void setPromptString();
 void inputArgs();
 bool checkCommandExist(string scmd);
 void cmdExecFuntion(string incommingCommd);
+void getHistory();
+void addNewVariable(string newVariable);
+string removeExport(string initCommand,string settingStr);
+int searchStr(string searchString);
+int updateOldVariable(string oldString,string newString);
